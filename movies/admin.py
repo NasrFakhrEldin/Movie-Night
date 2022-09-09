cc7807fd15b126943cc81b7b46a049583374c84d
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-09-09 14:32:25 +0200

    omdb api

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from movies.models import Genre, Movie, SearchTerm
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(SearchTerm)
