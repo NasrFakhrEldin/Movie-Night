e8c77386eca9a502292e30d17d775f24f3372258
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-09-10 10:22:36 +0200

    custom User model - set base templates

--- movienight/settings.py
+++ movienight/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+AUTH_USER_MODEL = "movienight_auth.User"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # AUTH_APP
+    'movienight_auth',
 
     #App
     'movies',
