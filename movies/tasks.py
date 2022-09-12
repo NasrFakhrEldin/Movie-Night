ca756851d3a9f964df68e12abe34704709a43273
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-09-12 10:01:35 +0200

    add some tasks - celery

---
+++ movies/tasks.py
@@ -0,0 +1,22 @@
+from celery import shared_task
+from movies import notifications
+from movies.models import MovieNightInvitaion
+
+@shared_task
+def send_invitaion(mni_pk):
+    notifications.send_invitation(
+        MovieNightInvitaion.objects.filter(pk=mni_pk)
+    )
+
+
+@shared_task
+def send_attendance_change(mni_pk, is_attending):
+    notifications.send_attendance_change(
+        MovieNightInvitaion.objects.get(pk=mni_pk),
+        is_attending
+    )
+
+
+@shared_task
+def notify_of_starting_soon():
+    notifications.notify_of_starting_soon()
